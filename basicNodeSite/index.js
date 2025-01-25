f460ea6749f42fdb393eb0472ab555a1eaf7b3c7
pkhobunsongserm <pkhobunsongserm@gmail.com>
2025-01-25 11:46:06 +1100

    completed node.js basics

---
+++ basicNodeSite/index.js
@@ -0,0 +1,29 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "";
+
+    console.log(q.pathname);
+
+    if (q.pathname !== "/") {
+      filename = "." + q.pathname + ".html";
+    } else {
+      filename = "./index.html";
+    }
+
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      console.log(data);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
